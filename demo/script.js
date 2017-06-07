9c9eb0be58b6ddf263970b1057af10e76f45e2bd
ewwink <ewwink@yahoo.com>
2017-06-07 18:16:13 +0700

    reset

--- demo/script.js
+++ demo/script.js
@@ -1,8 +1,8 @@
 var tryNumber = 0;
-document.getElementById("testDebug").addEventListener("click", function(){
+document.getElementById("noDebug").addEventListener("click", function(){
   tryNumber++;
   var changeMe = "Breakpoint not set " + tryNumber;
-  document.getElementById("noDebug").innerHTML = changeMe ;                                           
+  document.getElementById("debugResult").innerHTML = changeMe ;                                           
 });
 
 document.getElementById("testDebug").addEventListener("click", function(){
